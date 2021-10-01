d8add7c9caa7f405fd1a6ebd6e630b2f53db77ae
Frenzis <fra.tossi@gmail.com>
2021-10-01 14:59:47 +0200

    apache log reader v0.1

--- log_reader.py
+++ log_reader.py
@@ -1,6 +1,9 @@
 import matplotlib
+from pandas.core.indexes.base import Index
 matplotlib.use("Agg")
 
+import io
+import base64
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
@@ -47,9 +50,6 @@ while entries > i :
     i += 1
 
 
-
-
-
 ##dictionary of IPs counts
 ips_data = {x:list_ips.count(x) for x in list_ips}
 busy_dates = {x:list_dates.count(x) for x in list_dates}
@@ -73,13 +73,30 @@ while x < 10 :
 df = pd.DataFrame.from_records(lines)
 df.columns = ["IPs", "Time", "Operation", "Reply", "User-Agent"]
 
+#user agent type
+useragent = df.groupby('User-Agent').count()
+useragent = useragent.sort_values(by=['IPs'], ascending=False)
+total_requests = useragent.sum(axis=1)
+totalc = useragent.sum()
+totalc = totalc.iloc[0]
+#user agent operation
+useragent_operation = df.groupby('Operation').count()
+useragent_operation = useragent_operation.sort_values(by=['IPs'], ascending=False)
+#print(useragent_operation.index[0], 'index')
+#print(useragent_operation.loc[useragent_operation.index[0], 'IPs'])
+#top 3 user agents
+user_agent = [total_requests.index[0], total_requests.index[1], total_requests.index[2]]
+user_agent.append('Other')
+user_agent_conn = [total_requests.iloc[0], total_requests.iloc[1], total_requests.iloc[2]]
+#print(total_requests.loc[total_requests.index[1], 'User-Agent'])
+#print(useragent_operation.index[0])
 #recurrent IPs
 df2 = pd.DataFrame(first_ten)
 df2.columns = ["IPs", "Connections"]
 #days
 df3 = pd.DataFrame(busy_dates)
 df3.columns = ["Dates", "Connections"]
-#print(df3)
+
 
 ######Graphs
 
@@ -95,60 +112,176 @@ plt.xticks(rotation=45)
 app = dash.Dash(__name__)
 
 
-
-def create_IP_graph():
-    fig, ax = plt.subplots()
-    fig.set_dpi(75)
-    #fig.set_size_inches(int(i), int(i))
-
-    ax.bar(df2["IPs"],df2["Connections"])
-    plt.xlabel('IPs')
-    plt.ylabel('Connections')
-    plt.title('Highest 10 IP\'s connections')
-    return mpl_to_plotly(fig)
-
-
-def create_daily_graph():
-    fig, ax = plt.subplots()
-    fig.set_dpi(75)
-    #fig.set_size_inches(int(i), int(i))
-    ax.bar(df3["Dates"],df3["Connections"])
-    plt.xlabel('days')
-    plt.ylabel('Connections')
-    plt.title('Logged connections per day ')
-    return mpl_to_plotly(fig)
-
-def create_round_graph():
-    pass
-
-
 app.layout = html.Div(
     children=[
         html.Div(className='row',
-                 children=[
-                    html.Div(className='four columns div-user-controls',
-                             children=[
-                                 html.H2('DASH - APACHE LOGS READER'),
-                                 html.P('Visualising apache.'),
-                                 html.P('Pick filters from the dropdown below.'),
-                                 
-                                ]
-                             ),
-                    html.Div(className='eight columns div-for-charts bg-grey',
-                             children=[
-                                 html.H2('Top IP connections:'),
-                                 html.P(ips_data[0][0]),
-                                 dcc.Graph(figure=create_IP_graph()),
-                                 html.P('Busiest day: '),
-                                 html.P(busy_dates[0][0]),
-                                 dcc.Graph(figure=create_daily_graph()),
-                             ]),
-                              ])
-        ]
+                 children=[ 
+                     html.Div(className='four columns div-for-charts bg-grey', 
+                        children=[  
+                                    html.H1('DASH - APACHE LOGS READER'), 
+                                    html.P('Visualising apache logs, Pick filters from the dropdown below.'), 
+                                    html.P(''), 
+                                    html.P('_____________________________________'), 
+                                    html.P('Top connections Menu :'),
+                                    dcc.Dropdown(id="drop",
+                                        options=[
+                                            {'label': 'IPs connections', 'value': '0'},
+                                            {'label': 'Busiest Day', 'value': '1'},
+                                            ],
+                                        value='0',
+                                        clearable=False,
+                                        multi=False
+                                    ),
 
+                                    html.P('User Agent Menu :'),
+                                    dcc.Dropdown(id="top_3",
+                                        options=[
+                                            {'label': 'Top User Agents', 'value': '0'},
+                                            {'label': 'type of query', 'value': '1'},
+                                            ],
+                                        value='0',
+                                        clearable=False,
+                                        multi=False
+                                    ),
+                                ]
+                            ),
+                    
+                     html.Div(className='four columns div-for-charts ',
+                        children=[
+                                    html.H2('Top connections:'), 
+                                    html.Div(className='two columns div-user-controls', style={'Display': 'flex'}, 
+                                        children=[ 
+                                                    html.Img(id='example'),
+                                                ]
+                                        ),
+                                        html.P(id='text_one'),
+                                        
+                               ]
+                            ),
+
+
+                     html.Div(className='four columns div-for-charts', 
+                        children=[  
+                                    html.H2('User-Agets Stats'), 
+                                    html.Div(className='two columns div-user-controls', 
+                                        children=[ 
+                                                    html.Img(id='example3'),
+                                                ]
+                                        ), 
+                                    html.P('Top 3 user agents:'),
+                                    html.P(user_agent[0]),
+                                    html.P(user_agent[1]),
+                                    html.P(user_agent[2]), 
+                                ]
+                            ),               
+
+                         ]
+            ),
+        ]
 )
 
+@app.callback(
+    dash.dependencies.Output('text_one', 'children'), # src attribute
+    [dash.dependencies.Input('drop', 'value')]
+)
+def text_one(drop):
+    if int(drop) == 0 :
+        message = 'IPs by connections : \n' + ips_data[0][0] + " - \n" + ips_data[1][0] + " - \n" + ips_data[2][0]
+    else:
+        message = 'Most busy day : ', busy_dates[0][0]
 
+    return message 
+
+@app.callback(
+    dash.dependencies.Output('example', 'src'), # src attribute
+    [dash.dependencies.Input('drop', 'value')]
+)
+def create_IP_graph(drop):
+    fig, ax = plt.subplots()
+    fig.set_dpi(75)
+    #fig.set_size_inches(int(i), int(i))
+    if int(drop) == 0 :
+        ax.bar(df2["IPs"],df2["Connections"])
+        plt.xlabel('IPs')
+        plt.ylabel('Connections')
+        plt.title('Highest 10 IP\'s connections')
+    elif int(drop) == 1:
+        ax.bar(df3["Dates"],df3["Connections"])
+        plt.xlabel('days')
+        plt.ylabel('Connections')
+        plt.title('Logged connections per day ')
+            
+    plt.xticks(rotation=45)
+
+    buf = io.BytesIO() # in-memory files
+    fig.savefig(buf, format = "png") # save to the above file object
+    data = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
+    plt.close()
+    return "data:image/png;base64,{}".format(data)
+    
+# @app.callback(
+#     dash.dependencies.Output('example2', 'src'), # src attribute
+#     [dash.dependencies.Input('n_points', 'value')]
+# )
+# def create_daily_graph(n_points):
+#     fig, ax = plt.subplots()
+#     fig.set_dpi(75)
+#     #fig.set_size_inches(int(i), int(i))
+#     ax.bar(df3["Dates"],df3["Connections"])
+#     plt.xlabel('days')
+#     plt.ylabel('Connections')
+#     plt.title('Logged connections per day ')
+#     plt.xticks(rotation=45)
+
+#     buf2 = io.BytesIO() # in-memory files
+#     fig.savefig(buf2, format = "png") # save to the above file object
+#     data = base64.b64encode(buf2.getbuffer()).decode("utf8") # encode to html elements
+#     plt.close()
+#     return "data:image/png;base64,{}".format(data)
+
+
+@app.callback(
+    dash.dependencies.Output('example3', 'src'), # src attribute
+    [dash.dependencies.Input('top_3', 'value')]
+)
+def create_round_graph(top_3):
+    i=0
+    tmpv = 0
+    percent = []
+    names = []
+    if int(top_3) == 0:
+        while i < 3:
+            percent.append(user_agent_conn[i]/100)
+            names.append(total_requests.index[i])
+            tmpv += percent[i]
+            i += 1
+
+        tmpv = 100 -tmpv
+        names.append('Other')
+        percent.append(tmpv)
+    else:
+        while i < 3 :
+            temp = int(useragent_operation.loc[useragent_operation.index[i], 'IPs'])
+            percent.append(temp/100)
+            tmpv += percent[i]
+            names.append(total_requests.index[i])
+            i += 1
+        tmpv = 100 -tmpv
+        percent.append(tmpv)
+        names.append('Other')
+
+    fig, ax = plt.subplots()
+    fig.set_dpi(75)
+    explode = (0.1, 0, 0, 0) 
+    ax.pie(percent, explode=explode, labels=names, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax.axis('equal')
+    plt.title("Top 3 User-Agents")
+  
+    buf = io.BytesIO() # in-memory files
+    fig.savefig(buf, format = "png") # save to the above file object
+    data = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
+    plt.close()
+    return "data:image/png;base64,{}".format(data)
 
 
 
